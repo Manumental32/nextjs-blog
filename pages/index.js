3a791d3095e1940a04f6f5522554a5a42f2a23fb
Manuel Aquino <maquino@arsat.com.ar>
2022-05-30 14:05:25 -0300

    get postsdata in getStaticProps from page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
 import utilStyles from "../styles/utils.module.css";
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -11,8 +12,32 @@ export default function Home() {
 
       <section className={utilStyles.headingMd}>
         <p>Ingeniero en Sistemas de información</p>
-        <p>Podes contactarme en <a href='https://www.linkedin.com/in/aquinomanuel/' target="_blank">linkedin</a></p>
+        <p>
+          Podes contactarme en{" "}
+          <a href="https://www.linkedin.com/in/aquinomanuel/" target="_blank">
+            linkedin
+          </a>
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return { props: { allPostsData } };
 }
